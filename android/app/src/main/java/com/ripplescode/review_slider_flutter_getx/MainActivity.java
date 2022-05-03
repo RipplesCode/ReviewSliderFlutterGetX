b3001d854b715fc8ceec4b0b6017d014f918cf9a
RipplesCode <ripplescode@gmail.com>
2022-05-03 12:01:27 +0530

    first commit

---
+++ android/app/src/main/java/com/ripplescode/review_slider_flutter_getx/MainActivity.java
@@ -0,0 +1,6 @@
+package com.ripplescode.review_slider_flutter_getx;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+  var selectedValue = 1.obs;
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {}
+}
